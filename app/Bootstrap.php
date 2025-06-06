c3c77119e6eb9266739bac795d8ff7053473a747
Jimothy <142246142+RaptorV6@users.noreply.github.com>
2025-06-06 16:17:06 +0200

    localhost tracy

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,36 +1,37 @@
 <?php
-
+// app/Bootstrap.php
 declare(strict_types=1);
 
 namespace App;
 
 use Nette\Bootstrap\Configurator;
 
-
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
-		$appDir = dirname(__DIR__);
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
+        $appDir = dirname(__DIR__);
 
-		$configurator->setDebugMode(true); // enable for your remote IP
+        // ✅ Tracy jen pro lokální IP adresy
+        $configurator->setDebugMode(['127.0.0.1', '::1']); 
         $configurator->enableDebugger(__DIR__. '/../log');
-		$configurator->enableTracy($appDir . '/log');
+        $configurator->enableTracy($appDir . '/log');
 
-		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory($appDir . '/temp');
+        $configurator->setTimeZone('Europe/Prague');
+        $configurator->setTempDirectory($appDir . '/temp');
 
-		$configurator->createRobotLoader()
-			->addDirectory(__DIR__)
-			->register();
-                \Kdyby\Replicator\Container::register();
-		$configurator->addConfig($appDir . '/config/common.neon');
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
+            
+        \Kdyby\Replicator\Container::register();
+        $configurator->addConfig($appDir . '/config/common.neon');
                 
-                $configurator->onCompile[] = function ($configurator, $compiler) {
-                    $compiler->addExtension('dibiPostgre', new \Dibi\Bridges\Nette\DibiExtension22);
-                };
+        $configurator->onCompile[] = function ($configurator, $compiler) {
+            $compiler->addExtension('dibiPostgre', new \Dibi\Bridges\Nette\DibiExtension22);
+        };
 
-		return $configurator;
-	}
+        return $configurator;
+    }
 }
